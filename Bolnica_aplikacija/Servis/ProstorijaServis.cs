70bd7b378bf70a0d19669ed3e6140386a4143ccf
dusansisarica <dusansisarica@gmail.com>
2021-05-23 13:15:09 +0200

    jos jedna sitna izmena za cc

--- Bolnica_aplikacija/Servis/ProstorijaServis.cs
+++ Bolnica_aplikacija/Servis/ProstorijaServis.cs
@@ -398,7 +398,7 @@ namespace Bolnica_aplikacija.Servis
         private void premestiStatickuStavku(ProstorijaPrebacivanjeDTO prebacivanje)
         {
             var prostorijaIz = nadjiProstorijuPoId(prebacivanje.idProstorijeIzKojeSePrebacuje);
-            if (ZakaziPremestanje(prebacivanje))
+            if (ZakaziPremestanjeU(prebacivanje))
             {
                 kopirajProstorijuIUpisi(prostorijaIz);
                 return;
